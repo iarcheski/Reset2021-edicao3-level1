cc453708886b5371ef65b88449fc8532bf6d82b9
Débora <87210979+iarcheski@users.noreply.github.com>
2021-08-18 20:15:24 -0300

    Add files via upload

---
+++ AlunoResetComParametro.java
@@ -0,0 +1,9 @@
+public class AlunoResetComParametro {
+
+        public static void main(String[] args) {
+            String nome = "Débora";
+
+            System.out.println("Sou aluno do Level 1 do CWI Reset! Me chamo " + (nome));
+
+        }
+    }
