2e7dae45ec65103f7fd80c54350e5fc547d57ba1
Débora <87210979+iarcheski@users.noreply.github.com>
2021-08-19 15:42:05 -0300

    Delete AlunoResetComParametro.java

--- AlunoResetComParametro.java
+++
@@ -1,9 +0,0 @@
-public class AlunoResetComParametro {
-
-        public static void main(String[] args) {
-            String nome = "Débora";
-
-            System.out.println("Sou aluno do Level 1 do CWI Reset! Me chamo " + (nome));
-
-        }
-    }
